2bb29dc8bf6c66ab8ec3aff57e502c338da14a92
TihonToloconnicov <131188948+TihonToloconnicov@users.noreply.github.com>
2023-05-02 10:30:48 +0300

    Completed 1

--- HomeWork4Part1/HomeWork4Part1/ViewController.swift
+++ HomeWork4Part1/HomeWork4Part1/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var showButton: UIButton!
+    @IBOutlet weak var textOnDesplay: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textOnDesplay.text = "Hello"
     }
 
-
+    
+    @IBAction func Add(_ sender: Any) {
+        textOnDesplay.text! += "1XBET!!!"
+        
+    }
+    
 }
 
